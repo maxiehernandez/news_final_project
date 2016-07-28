f6f3159f4aa90f95cb07592968a4ff6d09261084
Maxie Hernandez <maxinehernandez@hotmail.com>
2016-07-28 19:13:24 -0400

    Added user login.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   root 'editors#dashboard'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+
+  get 'users/new'
+  get 'users/create'
+  get 'users/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
