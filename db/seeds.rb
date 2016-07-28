e7e98e7bd5915ed740b37d90e78498007208ca3b
Steven Gilliam <stevengilliamjr@gmail.com>
2016-07-27 21:08:05 -0400

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Editor.create(email: "test@me.com" , password_digest: "12345")
+
+Topic.create(name:"Election 2016")
+Topic.create(name:"US News")
+Topic.create(name:"Immigration")
+Topic.create(name:"BlackLivesMatter")
+Topic.create(name:"Breaking News")
+
+25.times do |n|
+  Story.create(name:"example#{n}")
+end
