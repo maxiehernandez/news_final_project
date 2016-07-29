857a0fdf2b867ed4b9472b65ba1f173d8f155908
Andrea Perez <andrea.ocarina@gmail.com>
2016-07-29 02:41:32 -0400

    Displayed stories on editor's dashboard.

--- app/models/rssutilities.rb
+++ app/models/rssutilities.rb
@@ -1,24 +1,25 @@
 require 'rss'
 
 class RSSUtilities
-  def initilize
-    @cnn_all = RSS::Parser.parse('http://rss.cnn.com/rss/cnn_topstories.rss', false)
-    @dem_now = RSS::Parser.parse('http://democracynow.org/democracynow.rss', false)
-    @blk_agenda = RSS::Parser.parse('http://blackagendareport.com/rss.xml', false)
+  class << self
+    def fetch
+      @cnn_all = RSS::Parser.parse('http://rss.cnn.com/rss/cnn_topstories.rss', false)
+      @dem_now = RSS::Parser.parse('http://democracynow.org/democracynow.rss', false)
+      @blk_agenda = RSS::Parser.parse('http://blackagendareport.com/rss.xml', false)
+    end
+
+    def build_rss_stories
+      @cnn_all.items.each do |item|
+        Story.create(body:"<li>#{item.title}</li><li>#{item.link}</li><li>#{item.pubdate}</li>")
+      end
+
+      @dem_now.items.each do |item|
+        Story.create(body:"<li>#{item.title}</li><li>#{item.link}</li><li>#{item.pubdate}</li>")
+      end
+
+      @blk_agenda.items.each do |item|
+        Story.create(body:"<li>#{item.title}</li><li>#{item.link}</li><li>#{item.pubdate}</li>")
+      end
+    end
   end
-
-  def build_rss_stories
-    @cnn.items.each do |item|
-      Story.create(body:"<li>#{item.title}</li><li>#{item.link}</li><li>#{item.pubdate}</li>")
-    end
-
-    @dem_now.items.each do |item|
-      Story.create(body:"<li>#{item.title}</li><li>#{item.link}</li><li>#{item.pubdate}</li>")
-    end
-
-    @blk_agenda.items.each do |item|
-      Story.create(body:"<li>#{item.title}</li><li>#{item.link}</li><li>#{item.pubdate}</li>")
-    end
-  end
-
 end
