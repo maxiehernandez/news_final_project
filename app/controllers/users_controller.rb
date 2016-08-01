c47f2c4b389338004050878a9191d86f3ef9d17d
Andrea Perez <andrea.ocarina@gmail.com>
2016-08-01 13:46:18 -0400

    merge with master

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,9 @@ class UsersController < ApplicationController
   end
 
   def get_feeds
-
     TwitterUtilities.save_story
     RSSUtilities.save_rss_stories
     select_rss
-    select_tw
-
   end
 
   def dashboard
@@ -35,25 +32,7 @@ class UsersController < ApplicationController
   def select_rss
     News_rss.last(10).each do |news|
       p news
-
       Story.create!(body: "<div class='media'><div class='media-body'><h2 class='media-heading'><a href='#{news[:url]}'>#{news[:headline]}</a></h2><p>VIA *NEED SOURCE* #{news[:pub_date]}</p></div><div class='media-left'><a href='#{news[:url]}'><img class='media-object' src='https://hd.unsplash.com/photo-1453227588063-bb302b62f50b'></a></div></div>", topic_id: 4, story_type: "RS")
-    end
-  end
-
-  # def select_rss
-  #   News_rss.all.limit(20).each do |news|
-  #     Story.create!(body: "<li>#{news[:pub_date]}</li><li>#{news[:headline]}</li><li>#{news[:url]}</li>", topic_id: 4, story_type: 'RS')
-  #   end
-  # end
-
-
-
-  def select_tw
-    TwitterUtilities.build_story
-  end
-
-
-      Story.create!(body: "<div class='media'><div class='media-body'><h2 class='media-heading'><a href='#{news[:url]}'  target='_blank'>#{news[:headline]}</a></h2><p>VIA *NEED SOURCE* #{news[:pub_date]}</p></div><div class='media-left'><a href='#{news[:url]}' target='_blank'><img class='media-object' src='https://hd.unsplash.com/photo-1453227588063-bb302b62f50b'></a></div></div>", topic_id: 30)
     end
   end
 
