842be7759f320d13edcbdfb681d8fb0a2ae1b3f6
Maxie Hernandez <maxinehernandez@hotmail.com>
2016-08-03 21:05:39 -0400

    Added drag and drop feature for RSS Feed. RSS element is apending to the topic it is dropped into. Fixed tweets to append into the topic it is dropped into.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -9,28 +9,25 @@ class StoriesController < ApplicationController
   def create
     if params[:story_type] == "TW"
       @story = Story.create(
-        body: "<a href='https://twitter.com/#{params[:tweeters_id]}/status/#{params[:t_id]}'>Tweet!!!</a>",
+        body: "<a href='https://twitter.com/#{params[:tweeters_id]}/status/#{params[:t_id]}'></a>",
         topic_id: params[:topic_id],
         story_type: "TW")
-
-
-      if @story.save!
-        head :created
-      else
-        puts "ERROR!!!"
+      elsif params[:story_type] == "RS"
+        @story = Story.create(body: "<div class='media'><div class='media-body'><h2 class='media-heading'><a href='#{params[:url]}'>#{params[:headline]}</a></h2><p>VIA *NEED SOURCE* #{params[:pub_date]}</p></div><div class='media-left'><a href='#{params[:url]}'><img class='media-object' src='https://hd.unsplash.com/photo-1453227588063-bb302b62f50b'></a></div></div>", topic_id: params[:topic_id], story_type: "RS")
       end
-    end
-    # @topic =
-    # @story = @topic.stories.create(stories_params)
-    # @story.topic_id = @topic.id
 
+    if @story.save!
+      head :created
+    else
+      puts "ERROR!!!"
+    end
   end
+
 
   private
 
   def story_params
     params.require(:story).permit(:body, :topic_id)
   end
-
 
 end
