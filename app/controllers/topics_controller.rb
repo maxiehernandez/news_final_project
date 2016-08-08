bf496d7173c5945d85c106a03f3427ec78c21810
Steven Gilliam <stevengilliamjr@gmail.com>
2016-08-08 00:26:25 -0400

    catching up to master

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -24,6 +24,7 @@ before_action :set_topic, only: [:show, :edit, :update, :destroy]
   end
 
   def trending
+    TwitterUtilities.save_story
     Soc_med.build_top_tweet_stories
     Soc_med.build_new_hotness
     Soc_med.top_retweets
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
     @soc_meds = Soc_med.all
     @soc_med = Soc_med.new
     # FeedlyFetcher.fetch
-    TwitterUtilities.save_story  # saves Tweets from Twitter API into Soc_med
+    # TwitterUtilities.save_story  # saves Tweets from Twitter API into Soc_med
     RssFeed.save_rss_images
     # RSSUtilities.save_rss_stories #saves RSS stories from feeds into News_rss
 
