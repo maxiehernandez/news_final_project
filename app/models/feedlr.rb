a59e17c6f0fa3cc75b27955163d50e7046e95724
Maxie Hernandez <maxinehernandez@hotmail.com>
2016-08-08 15:56:45 -0400

    commented out unused feedlr class

--- app/models/feedlr.rb
+++ app/models/feedlr.rb
@@ -1,19 +1,19 @@
 # require 'feedlr'
-class Feedlr
-  class << self
-    def client
-      @client ||= Feedlr::Client.new(oauth_access_token:ENV['FEEDLY_KEY'])
-    end
-
-    def categories
-      client.user_categories
-    end
-  # Fetch user categories
-  #   Fetch user subscriptions
-    # p client.user_subscriptions
-
-    def save_feedlies
-    end
-
-  end
-end
+# class Feedlr
+#   class << self
+#     def client
+#       @client ||= Feedlr::Client.new(oauth_access_token:ENV['FEEDLY_KEY'])
+#     end
+#
+#     def categories
+#       client.user_categories
+#     end
+#   # Fetch user categories
+#   #   Fetch user subscriptions
+#     # p client.user_subscriptions
+#
+#     def save_feedlies
+#     end
+#
+#   end
+# end
